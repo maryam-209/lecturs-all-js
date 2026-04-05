207fa02a9741a2c271ddacbae42cbcd9f9eb5f19
Maryam Imran <maryammaryamimran97@gmail.com>
2026-04-05 21:53:13 +0500

    add files

---
+++ index.js
@@ -0,0 +1,28 @@
+//* array in js
+//* refrence type main object array or functions aty hy
+//& refrence type main data aheap memeory main hota hy lakin jo is ka refrence hy w stack memory main hota hy
+//* object is just a key valuue pair key and value 
+// let obj={
+//     fullname:"maryam",
+//     age:23,
+//     weight:75,
+//     height:"5fit",
+//     greet:function(){
+//         console.log("Noor parh bi liya karo kabii tum");
+//     }
+// };
+// console.log(obj);
+// obj.greet();
+// let obj2=obj;
+// console.log(obj2,"Main shallow copy ho");
+// let obj3=obj;
+// obj3.age=10;
+// console.log(obj3,"main deep ho");
+// console.log(obj,"main deep ho");
+//! array is a collection of items
+let ar=[1,2,3,4,5,6]
+console.log(ar,"done");
+let br=new Array(10,'maryam','hira',90);
+console.log(br[0]);//* ye constructor array hy ye tricky hota hy
+
+
